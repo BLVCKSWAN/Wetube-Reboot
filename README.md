c56bcb0e1ab7743b954c8e445cc049bd3ec1ca61
blvckswan <93452543+BLVCKSWAN@users.noreply.github.com>
2022-04-08 14:28:29 +0900

    #4.1

---
+++ README.md
@@ -0,0 +1,15 @@
+# Wetube Reboot
+
+/ -> home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/edit -> Edit User
+/users/delte -> Delete User
+
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete => Delete Video
+/videos/comments -> Comment on a video
+/videos/comments/delete -> Delete A Comment of a video
--- src/server.js
+++ src/server.js
@@ -1,22 +1,33 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
 
-const routerLogger = (req, res) => {
-  next();
-};
+const globalRouter = express.Router();
 
-const methodLogger = (req, res, next) => {
-  next();
-};
+const handleHome = (req, res) => res.send("Home");
 
-const home = (req, res) => {
-  return res.send("Hello!");
-};
+globalRouter.get("/", handleHome);
 
-app.get("/", methodLogger, routerLogger, home);
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`✅Server listening on port http://localhost:${PORT} 🚀`);
