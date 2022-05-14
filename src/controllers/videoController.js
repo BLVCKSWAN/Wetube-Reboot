c0953dc8985f8778f77c5e183544c2b23a4fcf9d
blvckswan <93452543+BLVCKSWAN@users.noreply.github.com>
2022-05-14 11:19:18 +0900

    #6.23

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -59,7 +59,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title: title,
       description: description,
-      hashtags: hashtags.split(",").map((word) => `#${word}`),
+      hashtags,
     });
     return res.redirect("/");
   } catch (error) {
